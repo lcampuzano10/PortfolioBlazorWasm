118aeff0ad0dd8fabdd5a67b7b794cc67ee5412b
Luis C Casa <luis.campuzano32@hotmail.com>
2023-09-06 22:29:32 -0400

    Adding some changes to company Name

--- PortfolioAndBlog/Models/MyProject.cs
+++ PortfolioAndBlog/Models/MyProject.cs
@@ -2,7 +2,8 @@
 {
 	public class MyProject : Identifier
 	{
-		public string? ProjectPicturePath { get; set; }
+        public string Company { get; set; }
+        public string? ProjectPicturePath { get; set; }
 		public string? DataTextModal { get; set; }
 		public string? ProjectName { get; set; }
 		public string? ProjectNameDescription { get; set; }
--- PortfolioAndBlog/Models/MyProjectsPage.cs
+++ PortfolioAndBlog/Models/MyProjectsPage.cs
@@ -6,6 +6,6 @@
 		public string? HeadingPart2 { get; set; }
 		public string? InfoHeading { get; set; }
 		public ICollection<MyProject>? Projects { get; set; }
-		
+
 	}
 }
